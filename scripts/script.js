544dc97ad0baa75991039e47d2a6e0ffa9d53456
MyNameJuju <65947858+MyNameJuju@users.noreply.github.com>
2021-06-01 16:42:06 +0700

    update js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
   var firstImg = $('#1');
-  var lastImg = $('#3')
+  var secondImg = $('#2');
+  var lastImg = $('#3');
+  var firstDot = $('#dot1');
+  var secondDot = $('#dot2');
+  var thirdDot = $('#dot3');
   $('.next').on('click', function(){
-    var i = 0;
     var currentImg = $('.active');
     var nextImg = currentImg.next();
+    var currentDot = $('.active');
+    var nexttDot = currentDot.next();
     
     if(nextImg.length){
       currentImg.removeClass('active').css('z-index', -10);
       nextImg.addClass('active').css('z-index', 10);
+      currentDot.removeClass('active').css('z-index', -10);
+      nexttDot.addClass('active').css('z-index', 10);
     }
     else{
       currentImg.removeClass('active').css('z-index', -10);
       firstImg.addClass('active').css('z-index', 10);
+      currentDot.removeClass('active').css('z-index', -10);
+      firstDot.addClass('active').css('z-index', 10);
     }
   console.log('next clicked');
   });
@@ -20,17 +29,55 @@ $(document).ready(function(){
   $('.prev').on('click', function(){
     var currentImg = $('.active');
     var prevImg = currentImg.prev();
+    var currentDot = $('.active');
+    var prevDot = currentDot.prev();
 
     if(prevImg.length){
       currentImg.removeClass('active').css('z-index', -10);
       prevImg.addClass('active').css('z-index', 10);
+      currentDot.removeClass('active').css('z-index', -10);
+      prevDot.addClass('active').css('z-index', 10);
     }
     else{
       currentImg.removeClass('active').css('z-index', -10);
       lastImg.addClass('active').css('z-index', 10);
+      currentDot.removeClass('active').css('z-index', -10);
+      thirdDot.addClass('active').css('z-index', 10);
     }
   console.log('prev clicked');
   });
+
+  $('.dot1').on('click', function(){
+    var currentImg = $('.active');
+    var currentDot = $('.active');
+    // currentImg.removeClass('active').css('z-index', -10);
+    firstImg.addClass('active').css('z-index', 10);
+    currentDot.removeClass('active').css('z-index', -10);
+    firstDot.addClass('active').css('z-index', 10);
+    console.log('first dot clicked');
+  });
+
+  $('.dot2').on('click', function(){
+    var currentImg = $('.active');
+    var currentDot = $('.active');
+    currentImg.removeClass('active').css('z-index', -10);
+    secondImg.addClass('active').css('z-index', 10);
+    currentDot.removeClass('active').css('z-index', -10);
+    secondDot.addClass('active').css('z-index', 10);
+    console.log('second dot clicked');
+  });
+
+  $('.dot3').on('click', function(){
+    var currentImg = $('.active');
+    var currentDot = $('.active');
+    currentImg.removeClass('active').css('z-index', -10);
+    lastImg.addClass('active').css('z-index', 10);
+    currentDot.removeClass('active').css('z-index', -10);
+    thirdDot.addClass('active').css('z-index', 10);
+    console.log('third dot clicked');
+  });
+
 });
+  
 
 
