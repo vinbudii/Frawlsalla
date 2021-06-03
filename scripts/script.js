4182a1c8963d0b4694d90963152e6e43c074ce76
MyNameJuju <65947858+MyNameJuju@users.noreply.github.com>
2021-06-03 17:26:14 +0700

    Update script.js
    sticky

--- scripts/script.js
+++ scripts/script.js
@@ -77,7 +77,20 @@ $(document).ready(function(){
     console.log('third dot clicked');
   });
 
+  window.onscroll = function() {stickyHeader()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 });
-  
-
-
